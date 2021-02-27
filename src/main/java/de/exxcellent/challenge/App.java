7b7076970a24c096b4f434db49ca04cebdee5539
ml78baky <maike.latsch@googlemail.com>
2021-02-27 15:08:38 +0100

    call dayWithSmallestTempSpread in entry class

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,7 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.analysis.*;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -14,9 +16,9 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        WeatherAnalysis weatherAnalysis = new WeatherAnalysis();
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = weatherAnalysis.getDayWithSmallestTemperatureSpread("src/main/resources/de/exxcellent/challenge/weather.csv");
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
